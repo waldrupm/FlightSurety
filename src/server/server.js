d7252752fa10693e288f6fc192ead81bc6dde3c4
Micheal Waldrup <waldrupm@gmail.com>
2019-08-04 14:38:38 -0400

    Add OracleRegistration to server, fix gas issues.

--- src/server/server.js
+++ src/server/server.js
@@ -14,15 +14,15 @@ let flightSuretyData = new web3.eth.Contract(FlightSuretyData.abi, config.dataAd
 // State maintenance
 let truffleAccounts = [];
 const possibleStatus = [0, 10, 20, 30, 40, 50];
-let oracleMap = new Map();
+let oraclesMap = new Map();
 
 
 // SETUP
 const setupOracles = async () => {
   //Get truffle accounts
   truffleAccounts = await getTruffleAccounts();
-  //Register Oracles the last 20 of them
-
+  //Register Oracles to the last 20 of them and assign indexes
+  registerAllOracles();
   //Setup an airline and fund it
 
   //Register a flight or two
@@ -36,12 +36,54 @@ const getTruffleAccounts = async () => {
       if (e) {
         reject(error);
       } else {
-        console.log(res);
         resolve(res);
       }
     });
   });
 };
+
+const registerOracle = (oracle) => {
+  return new Promise ((resolve, reject) => {
+    flightSuretyApp.methods.registerOracle().send({from: oracle, value: web3.utils.toWei("1", "ether"), gas: 220000000000}, (e, res) => {
+      if (e) {
+        console.log(e);
+        reject(e);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+};
+
+const registerAllOracles = async () => {
+  for (let a = 20; a < 40; a++) {
+    console.log(truffleAccounts[a]);
+    await checkGasRequirement(truffleAccounts[a]);
+    await registerOracle(truffleAccounts[a]);
+    console.log("Oracle registered", a-19);
+    let oracleIndexes = await getIndexes(truffleAccounts[a]);
+    console.log(oracleIndexes);
+    oraclesMap.set(truffleAccounts[a], oracleIndexes);
+  }
+}
+
+const checkGasRequirement = (oracle) => {
+    flightSuretyApp.methods.registerOracle().estimateGas({from: oracle}).then(function(gasAmount) {
+      console.log(gasAmount);
+    });
+}
+
+const getIndexes = (oracle) => {
+  return new Promise( (resolve, reject) => {
+    flightSuretyApp.methods.getMyIndexes().call({from: oracle}, (e, res) => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
 
 // Initiate Setup
 setupOracles();
--- truffle-config.js
+++ truffle-config.js
@@ -4,11 +4,9 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     }
   },
   compilers: {
