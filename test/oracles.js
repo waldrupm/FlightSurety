6bf303744dc4b26fc513112ec81ed3634f9409df
Micheal Waldrup <waldrupm@gmail.com>
2019-08-07 13:19:33 -0400

    Fix oracle requests. start oracle response handling

--- test/oracles.js
+++ test/oracles.js
@@ -1,22 +1,23 @@
-
 var Test = require('../config/testConfig.js');
-//var BigNumber = require('bignumber.js');
+const truffleAssert = require('truffle-assertions');
+const BigNumber = require('bignumber.js');
 
 contract('Oracles', async (accounts) => {
 
   const TEST_ORACLES_COUNT = 20;
-  var config;
+  let config;
+  // Watch contract events
+  const STATUS_CODE_UNKNOWN = 0;
+  const STATUS_CODE_ON_TIME = 10;
+  const STATUS_CODE_LATE_AIRLINE = 20;
+  const STATUS_CODE_LATE_WEATHER = 30;
+  const STATUS_CODE_LATE_TECHNICAL = 40;
+  const STATUS_CODE_LATE_OTHER = 50;
+  
   before('setup contract', async () => {
     config = await Test.Config(accounts);
 
-    // Watch contract events
-    const STATUS_CODE_UNKNOWN = 0;
-    const STATUS_CODE_ON_TIME = 10;
-    const STATUS_CODE_LATE_AIRLINE = 20;
-    const STATUS_CODE_LATE_WEATHER = 30;
-    const STATUS_CODE_LATE_TECHNICAL = 40;
-    const STATUS_CODE_LATE_OTHER = 50;
-
+    
   });
 
 
@@ -36,17 +37,23 @@ contract('Oracles', async (accounts) => {
   it('can request flight status', async () => {
     
     // ARRANGE
-    let flight = web3.utils.fromAscii('ND1309'); // Course number
-    let timestamp = Math.floor(Date.now() / 1000);
+    let flight = web3.utils.utf8ToHex("Mike101");
+    let timestamp = 1564765898826;
 
     // Submit a request for oracles to get status information for a flight
-    await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
+    let tx = await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
+    truffleAssert.eventEmitted(tx, 'OracleRequest', (event) => {
+      return expect(web3.utils.hexToUtf8(event.flight)).to.equal("Mike101") &&
+              expect(event.airline).to.deep.equal(config.firstAirline) &&
+              expect(new BigNumber(event.timestamp).toNumber()).to.deep.equal(timestamp);
+    });
     // ACT
 
     // Since the Index assigned to each test account is opaque by design
     // loop through all the accounts and for each account, all its Indexes (indices?)
     // and submit a response. The contract will reject a submission if it was
     // not requested so while sub-optimal, it's a good test of that feature
+    let ctr = 1;
     for(let a=1; a<TEST_ORACLES_COUNT; a++) {
 
       // Get oracle information
@@ -54,12 +61,17 @@ contract('Oracles', async (accounts) => {
       for(let idx=0;idx<3;idx++) {
 
         try {
+          // TODO: fix this
           // Submit a response...it will only be accepted if there is an Index match
-          await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
+          let tx = await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
+          truffleAssert.eventEmitted(tx, 'OracleReport', (event) => {
+            console.log(event);
+          });
         }
         catch(e) {
           // Enable this when debugging
-           console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
+           console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp, ctr, e);
+           ctr++;
         }
 
       }
